4de5b567b9e5f9004266258d924c183fee54ddff
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-10-16 16:34:05 +0530

    16-10-2023

---
+++ module-1/task_02.dart
@@ -0,0 +1,14 @@
+//2. Write a program to make addition, Subtraction, Multiplication and Division of Two Numbers.
+void main(){
+  int a=100;
+  int b=22;
+  int sum=a+b;
+  int sub=a-b;
+  int mul=a*b;
+  double div=a/b;
+  print(sum);
+  print(sub);
+  print(mul);
+  print(div);
+
+}
---
+++ module-1/task_04.dart
@@ -0,0 +1,7 @@
+//4. Write a program to find the Area of Circle
+void main(){
+  double radius =8;
+  double pie=3.14,area;
+  area=pie*radius*radius;
+  print("area of circle is $area");
+}
