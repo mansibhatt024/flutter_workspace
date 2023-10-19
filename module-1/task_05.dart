333d672b119978dfdcd09fdfdb8fa3fd6c03e9c0
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-10-19 17:35:32 +0530

    Delete module-1 directory

--- module-1/task_05.dart
+++
@@ -1,7 +0,0 @@
-//5. Write a program to find the Area of Triangle
-void main(){
-  double base=5;
-  double height=8;
-  var area=0.5*base*height;
-  print("area of triangle is:$area");
-}
--- module-1/task_06.dart
+++
@@ -1,8 +0,0 @@
-//6. Write a program to find the simple Interest.
-void main(){
-  int principal=25;
-  int rate =10;
-  int number=8;
- double interest=principal*rate*number/100;
-  print('Simple interest is: $interest');
-}
