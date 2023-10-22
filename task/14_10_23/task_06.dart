207ca248ce6dc0f4670222ee6a86eedfaf63c998
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-10-22 14:59:40 +0530

    Add files via upload

---
+++ task/14_10_23/task_06.dart
@@ -0,0 +1,8 @@
+//6. Write a program to find the simple Interest.
+void main(){
+  int principal=25;
+  int rate =10;
+  int number=8;
+ double interest=principal*rate*number/100;
+  print('Simple interest is: $interest');
+}
