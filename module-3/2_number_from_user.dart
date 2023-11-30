e95a25338aa254e9146f9f01881c1da1644832b1
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-11-30 15:08:09 +0530

    Add files via upload

---
+++ module-3/2_number_from_user.dart
@@ -0,0 +1,105 @@
+// 3. Create an application to input 2 numbers from user and all numbers
+// between those 2 numbers in next activity
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputNumbersPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class InputNumbersPage extends StatefulWidget {
+  @override
+  _InputNumbersPageState createState() => _InputNumbersPageState();
+}
+
+class _InputNumbersPageState extends State<InputNumbersPage> {
+  TextEditingController firstNumberController = TextEditingController();
+  TextEditingController secondNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Enter Numbers'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: firstNumberController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Enter First Number'),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              controller: secondNumberController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Enter Second Number'),
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                int firstNumber = int.tryParse(firstNumberController.text) ?? 0;
+                int secondNumber = int.tryParse(secondNumberController.text) ?? 0;
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DisplayNumbersPage(
+                      firstNumber: firstNumber,
+                      secondNumber: secondNumber,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Show Numbers'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DisplayNumbersPage extends StatelessWidget {
+  final int firstNumber;
+  final int secondNumber;
+
+  DisplayNumbersPage({required this.firstNumber, required this.secondNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    List<int> numbers = [];
+    for (int i = firstNumber; i <= secondNumber; i++) {
+      numbers.add(i);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Numbers Display'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Numbers between $firstNumber and $secondNumber:'),
+            SizedBox(height: 16),
+            Text(numbers.toString()),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ module-3/reversenumber.dart
@@ -0,0 +1,142 @@
+// Create an application to take input number from user and print its
+// reverse number in TextField
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: ReversePrintPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class ReversePrintPage extends StatefulWidget {
+  const ReversePrintPage({Key? key}) : super(key: key);
+
+  @override
+  State<ReversePrintPage> createState() => _ReversePrintPageState();
+}
+
+class _ReversePrintPageState extends State<ReversePrintPage> {
+  TextEditingController numberController = TextEditingController();
+  TextEditingController resultController = TextEditingController();
+  int reversedNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Reverse Number App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            Text(
+              "Value of Reverse Printing.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.redAccent,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 70),
+            TextFormField(
+              controller: numberController,
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.black26,
+                fontWeight: FontWeight.normal,
+              ),
+              decoration: InputDecoration(
+                labelText: "Enter number",
+                prefixIcon: const Icon(
+                  CupertinoIcons.number_circle_fill,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                hintText: "54321",
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                ),
+                onPressed: () async {
+                  if (numberController.text.isEmpty) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Error"),
+                          content: const Text(
+                            "Please Enter the Value...",
+                            style: TextStyle(color: Colors.red, fontSize: 18),
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text("OK"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+              print(numberController.text);
+              int reversedNumber =
+              await reversePrinting(numberController.text);
+              setState(() {
+              resultController.text = reversedNumber.toString();
+              }
+              );
+                 }
+                            },
+                child: Text("SUBMIT"),),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              readOnly: true,
+              controller: resultController,
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.green,
+                fontWeight: FontWeight.normal,
+              ),
+              decoration: InputDecoration(
+                labelText: "Result of Reverse Printing",
+                prefixIcon: const Icon(
+                  CupertinoIcons.number_circle_fill,
+                  color: Colors.black,
+                  size: 25,
+                ),
+                hintText: "Reversed Value",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<int> reversePrinting(String value) async {
+    String numberString = value.toString();
+    int i = numberString.length - 1;
+    reversedNumber = 0;
+    while (i >= 0) {
+      int digit = int.parse(numberString[i]);
+      reversedNumber = reversedNumber * 10 + digit;
+      i--;
+    }
+    return reversedNumber;
+  }
+}
