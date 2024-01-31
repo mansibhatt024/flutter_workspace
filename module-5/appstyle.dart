7364610bc11b5fb043b7b0c4bb91395973f40a55
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2024-01-31 14:55:58 +0530

    Update appstyle.dart

--- module-5/appstyle.dart
+++ module-5/appstyle.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 
 import 'appcolors.dart';
 
-class CustomStyle{
-  static AppStyle({double fontSize = 16,Color color = primeColor,FontWeight fontWeight = FontWeight.w600}){
-    TextStyle textStyle = TextStyle(color: color,fontSize: fontSize,fontWeight:fontWeight);
+class CustomStyle {
+  static AppStyle(
+      {double fontSize = 16,
+        Color color = primeColor,
+        FontWeight fontWeight = FontWeight.w600}) {
+    TextStyle textStyle =
+    TextStyle(color: color, fontSize: fontSize, fontWeight: fontWeight);
     return textStyle;
   }
 }
